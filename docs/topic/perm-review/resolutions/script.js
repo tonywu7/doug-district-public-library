321fa689988a36ec0ad0fd3469589030689778e7
tonyzbf <tonyzbf@protonmail.com>
2021-05-21 18:34:12 -0400

    Create perm revamp plan (WIP)

---
+++ docs/topic/perm-review/resolutions/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+    tocbot.init({
+        // Where to render the table of contents.
+        tocSelector: '.toc',
+        // Where to grab the headings to build the table of contents.
+        contentSelector: 'article',
+        // Which headings to grab inside of the contentSelector element.
+        headingSelector: 'h1, h2, h3',
+        // For headings inside relative or absolute positioned containers within content.
+        hasInnerContainers: true,
+        collapseDepth: 3,
+    })
+})
